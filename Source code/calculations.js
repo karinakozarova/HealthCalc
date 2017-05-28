c7597c3009cfe99e1de8c0a629c4d1a0e2881860
Karina Kozarova <karinakozarova1303@gmail.com>
2017-05-28 15:39:31 +0300

    Again

--- Source code/calculations.js
+++ Source code/calculations.js
@@ -9,14 +9,11 @@ function Calculate()
             //finding if user is male or female
          	if (document.getElementById("sex_female").checked == true) {
          	    sex = 2;
-         	}
-         	
-         	else if (document.getElementById("sex_male").checked == true){
+         	} else if (document.getElementById("sex_male").checked == true){
          	    sex = 1;
-         	}
-         	else{
+         	} else{
          	    alert("Enter gender!");
-         	    Calculations();
+         	    Calculations(); // reccursion
          	}
          	
          	//BMI calculation
@@ -33,35 +30,26 @@ function Calculate()
          	    BMR = 10 * weight + 6.25 * height - 5 * age - 161; //woman
          	}
          
-         	//Needeed calorie intake 
-         
-         	//Activity level calculations
+         	//Needeed calorie intake based on the activity level calculations
          	var calorie_intake;;
          	
          	if (document.getElementById("rarely").checked == true) {
          	    calorie_intake = BMR * 1.2;
-         	}	
-         	else if (document.getElementById("often").checked == true){
+         	} else if (document.getElementById("often").checked == true){
          	    calorie_intake = BMR * 1.375;
-         	}
-         	else if (document.getElementById("very-often").checked == true){
+         	} else if (document.getElementById("very-often").checked == true){
          	    calorie_intake = BMR * 1.55;
-            }   
-         	else if (document.getElementById("everyday").checked == true){
+            } else if (document.getElementById("everyday").checked == true){
          	    calorie_intake = BMR * 1.95;
-         	}
-            else {
+         	} else {
                     alert("If you don't enter your training activity, we can't calculate your calorie intake.");
             }
       
          	//alert(calorie_intake);  //use only when debugging
          	
-           
-            //checking if user input is legit 	
-            //checking if user input is legit 	
+            //checking if user input is legit 		
          	if (document.getElementById("age").value > 0 && document.getElementById("age").value <= 120)
          	{
-         
          		if (height > 55 && height < 290)
          		{
          			if (weight > 15 && weight < 290)
@@ -70,13 +58,10 @@ function Calculate()
          				//For debugging only: 
          				//alert("Age:  " + document.getElementById("age").value + " Sex:  " + sex + "BMI: " + BMI);
          		        alert("Your BMI is " + BMI + ".\n" + "Your BMR is: " + BMR + ".\nYour calorie intake should be: " + calorie_intake);
-         			}
-         			else alert("Wrong weight input, please try again");
+         			} else alert("Wrong weight input, please try again");
          			
-         		}
-         		else alert("Wrong height input, please try again");
+         		} else alert("Wrong height input, please try again");
          		
-         	}
-         	else alert("Invalid age input, please try again");
+         	} else alert("Invalid age input, please try again");
          	
          }
