d4a39a03e0ddb530e8e4f70ed578905fe956ac87
Karina Kozarova <karinakozarova1303@gmail.com>
2017-05-28 15:33:39 +0300

    JS code changes

--- Source code/calculations.js
+++ Source code/calculations.js
@@ -1,46 +1,64 @@
- function Calculate()
+function Calculate()
          {
-         	// variables assignment
+         	//variables assignment
          	var sex = 1; // sex = 1 is male, 2 is female
          	var height = document.getElementById("height").value;
          	var weight = document.getElementById("weight").value;
          	var age = document.getElementById("age").value;
          
-         
-         	// BMI calculation
+            //finding if user is male or female
+         	if (document.getElementById("sex_female").checked == true) {
+         	    sex = 2;
+         	}
+         	
+         	else if (document.getElementById("sex_male").checked == true){
+         	    sex = 1;
+         	}
+         	else{
+         	    alert("Enter gender!");
+         	    Calculations();
+         	}
+         	
+         	//BMI calculation
          	var BMI = weight / (height * height) * 10000;
          	BMI = Math.round(BMI);
          
-         	// BMR calculation
+         	//BMR calculation
          	//This is based on the Mifflin - St Jeor equation. 
          	var BMR;
-         	if (sex == 1) BMR = 10 * weight + 6.25 * height - 5 * age + 5 //man
-         	else BMR = 10 * weight + 6.25 * height - 5 * age - 161 //woman
+         	if (sex == 1) {
+         	    BMR = 10 * weight + 6.25 * height - 5 * age + 5; //man
+         	}
+         	else {
+         	    BMR = 10 * weight + 6.25 * height - 5 * age - 161; //woman
+         	}
          
-         	// Needeed calorie intake 
+         	//Needeed calorie intake 
          
-         	// activity level calculations
-         	var activity_lvl;
-         	if (document.getElementById("rarely").checked == true) activity_lvl = 3;
-         	if (document.getElementById("often").checked == true) activity_lvl = 5;
-         	if (document.getElementById("very-often").checked == true) activity_lvl = 7;
-         	if (document.getElementById("everyday").checked == true) activity_lvl = 8;
-         
-         	var calorie_intake;
-         	//If you are not very active basal metabolic rate times 1.2. 	
-         	if (activity_lvl == 3) calorie_intake = BMR * 1.2;
-         	//Somewhat active people should multiply by 1.375. 
-         	if (activity_lvl == 5) calorie_intake = BMR * 1.375;
-         	//Some exercise during the week, the number is 1.55. 
-         	if (activity_lvl == 7) calorie_intake = BMR * 1.55;
-         	//If you do a lot of sports, you multiply by 1.95. 
-         	if (activity_lvl == 8) calorie_intake = BMR * 1.95;
-         
-         	// alert(calorie_intake);  //use only when debugging
+         	//Activity level calculations
+         	var calorie_intake;;
          	
-         	//checking if user input is legit
-         	if (document.getElementById("sex_female").checked == true) sex = 2;
-         	if (document.getElementById("sex_male").checked == true) sex = 1;
+         	if (document.getElementById("rarely").checked == true) {
+         	    calorie_intake = BMR * 1.2;
+         	}	
+         	else if (document.getElementById("often").checked == true){
+         	    calorie_intake = BMR * 1.375;
+         	}
+         	else if (document.getElementById("very-often").checked == true){
+         	    calorie_intake = BMR * 1.55;
+            }   
+         	else if (document.getElementById("everyday").checked == true){
+         	    calorie_intake = BMR * 1.95;
+         	}
+            else {
+                    alert("If you don't enter your training activity, we can't calculate your calorie intake.");
+            }
+      
+         	//alert(calorie_intake);  //use only when debugging
+         	
+           
+            //checking if user input is legit 	
+            //checking if user input is legit 	
          	if (document.getElementById("age").value > 0 && document.getElementById("age").value <= 120)
          	{
          
@@ -48,10 +66,10 @@
          		{
          			if (weight > 15 && weight < 290)
          			{
-         				//if here -> everything from the input is legit
-         				// For debugging only: 
+         		        //if here -> everything from the input is legit
+         				//For debugging only: 
          				//alert("Age:  " + document.getElementById("age").value + " Sex:  " + sex + "BMI: " + BMI);
-         				alert("Your BMI is " + BMI + ".\n" + "Your BMR is: " + BMR + ".\nYour calorie intake should be: " + calorie_intake);
+         		        alert("Your BMI is " + BMI + ".\n" + "Your BMR is: " + BMR + ".\nYour calorie intake should be: " + calorie_intake);
          			}
          			else alert("Wrong weight input, please try again");
          			
